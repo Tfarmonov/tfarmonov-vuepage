3f48eca13bac31a361632ef2b609b71067d5d1d5
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-24 00:11:24 -0400

    added api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,11 +5,9 @@
     <p v-if="fact">{{ fact }}</p>
   </div>
 </template>
+
 <script>
 export default {
-  props: {
-    msg: String,
-  },
   data() {
     return {
       fact: "",
@@ -24,22 +22,28 @@ export default {
           "X-RapidAPI-Host": 'facts-by-api-ninjas.p.rapidapi.com',
         },
       })
-        .then((response) => {
-          response.json().then((data) => {
-            this.fact = data[0].fact;
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.fact = data[0].fact;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     },
   },
 };
 </script>
+
 <style>
 button {
-padding: 12px 32px;
-font-size: 16px;
-border-radius: 8px;
+  padding: 12px 32px;
+  font-size: 16px;
+  border-radius: 8px;
+  cursor: pointer;
+  background-color: #42b983;
+  color: white;
+  border: none;
 }
 </style>
