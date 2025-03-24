793cb4a9117f87f7b8f0572ff0d1334c7a6eb95d
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-23 22:08:53 -0400

    correctional commit

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init    
 git add -A 
 git commit -m 'New deployment'
-git push -f git@github.com:Tfarmonov/tfarmonov-vuepage.git master:gh-pages
+git push -f git@github.com:Tfarmonov/tfarmonov-vuepage.git main:gh-pages
 
 cd -
