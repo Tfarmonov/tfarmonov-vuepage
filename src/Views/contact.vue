75055b1018dc65ec56bd4d79f7468ebc3038837e
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-30 13:31:34 -0400

    Update contact.vue

--- src/Views/contact.vue
+++ src/Views/contact.vue
@@ -1,4 +1,20 @@
 <!-- views/contact.vue -->
 <template>
-    <div><h1>contact</h1></div>
+    <goBack/>
+    <div>
+      <h1>Contact</h1>
+      <p>Name: Temurbek Farmonov</p>
+      <p>Email: Tmfarmonov@my.waketech.edu</p>
+    </div>
   </template>
+  <script>
+  import goBack from '@/components/goBack.vue'
+  export default {
+    name: "Contact",
+    components: {
+      goBack,
+    },
+  };
+
+
+</script>
