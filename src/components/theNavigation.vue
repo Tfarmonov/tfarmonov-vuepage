0c9a06194937a948ade1f7cc35da03b9eed2f42b
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-30 13:30:24 -0400

    Create theNavigation.vue

---
+++ src/components/theNavigation.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/random-facts">Random Facts</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </div>
+
+</template>
