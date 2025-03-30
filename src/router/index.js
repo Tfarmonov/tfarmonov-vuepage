a72af6b817e2da9c2f4c59a96f8bfed7ffcd7624
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-30 13:29:47 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'; 
 import VueRouter from 'vue-router'; 
-import Home from '../views/Home.vue'; 
-import About from '../views/About.vue'; 
+import Home from '@/views/Home.vue'; 
+import About from '@/views/About.vue'; 
+import Contact from '@/views/contact.vue';
+import RandomFacts from '@/views/Random-Facts.vue';
  
 Vue.use(VueRouter); 
 const routes = [ 
@@ -10,7 +12,16 @@ const routes = [
   }, 
   { 
       path: '/about', name: 'About', component: About 
-  } 
+  },
+  {
+      path: '/contact', name: 'Contact', component: Contact
+  },
+  {
+      path: '/Random-Facts', name: 'Random Facts', component: RandomFacts
+  },
+  {
+    path: '*', name: 'Not-Found', component: 'NotFound'
+  }
 ]; 
 const router = new VueRouter({ 
   mode: 'history', routes 
