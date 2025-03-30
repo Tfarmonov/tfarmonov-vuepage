9487d7046f3b8863d06397992b81f8e017e5cdb0
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-30 13:31:20 -0400

    Create not-found.vue

---
+++ src/Views/not-found.vue
@@ -0,0 +1,16 @@
+<!-- views/not-found.vue -->
+<template>
+  <goBack/>
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, that page doesn't exist.</p>
+  </div>
+</template>
+<script>  
+export default {
+  name: "NotFound",
+  components: {
+    goBack,
+  }, 
+};
+</script>
