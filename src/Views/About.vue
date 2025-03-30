da2fcd6f822e79b96b59739a4a4d408f3ad8c0d0
Temurbek Farmonov <141202017+Tfarmonov@users.noreply.github.com>
2025-03-30 13:32:20 -0400

    Update About.vue

--- src/Views/About.vue
+++ src/Views/About.vue
@@ -1,4 +1,14 @@
 <!-- views/About.vue -->
 <template>
+    <goBack/>
     <div><h1>About Page</h1></div>
   </template>
+  <script> 
+  import goBack from '@/components/goBack.vue'
+  export default {
+    name: "About",
+    components: {
+      goBack,
+    },
+  };
+  </script>
